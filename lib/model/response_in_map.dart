6cd32c2721f5152f6a13245c32bcf179ea6db03d
tejaskt <tejasktgeetanjali88@gmail.com>
2026-03-25 18:54:27 +0530

    Initial commit

---
+++ lib/model/response_in_map.dart
@@ -0,0 +1,74 @@
+class ResponseInMap {
+  ResponseInMap({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.accessToken,
+    required this.refreshToken,
+  });
+
+  final int? id;
+  final String? username;
+  final String? email;
+  final String? firstName;
+  final String? lastName;
+  final String? gender;
+  final String? image;
+  final String? accessToken;
+  final String? refreshToken;
+
+  ResponseInMap copyWith({
+    int? id,
+    String? username,
+    String? email,
+    String? firstName,
+    String? lastName,
+    String? gender,
+    String? image,
+    String? accessToken,
+    String? refreshToken,
+  }) {
+    return ResponseInMap(
+      id: id ?? this.id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      gender: gender ?? this.gender,
+      image: image ?? this.image,
+      accessToken: accessToken ?? this.accessToken,
+      refreshToken: refreshToken ?? this.refreshToken,
+    );
+  }
+
+  factory ResponseInMap.fromJson(Map<String, dynamic> json){
+    return ResponseInMap(
+      id: json["id"],
+      username: json["username"],
+      email: json["email"],
+      firstName: json["firstName"],
+      lastName: json["lastName"],
+      gender: json["gender"],
+      image: json["image"],
+      accessToken: json["accessToken"],
+      refreshToken: json["refreshToken"],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "username": username,
+    "email": email,
+    "firstName": firstName,
+    "lastName": lastName,
+    "gender": gender,
+    "image": image,
+    "accessToken": accessToken,
+    "refreshToken": refreshToken,
+  };
+
+}
